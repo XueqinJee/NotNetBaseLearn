663c714a438f147f62cd67e58d829cb82eb211d0
依莉雅 Today <2107885241@qq.com>
2025-03-23 21:52:51 +0800

    RagChat

---
+++ VectorStoreSearchDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+using VectorStoreSearchDemo;
+
+var host = Host.CreateApplicationBuilder();
+
+var kernelBuilder = host.Services.AddKernel();
+kernelBuilder.AddOpenAIChatCompletion(
+        "glm-4-flash",
+        "sk-8s0tnQTLm6UDj93lC0D1A2D38d1c4aE8A33a22B95597F72a",
+        "",
+        httpClient: new HttpClient() { BaseAddress = new Uri("http://localhost:3000/v1")}
+    );
+
+host.Services.AddHostedService<RagChatService>();
+
+var builder = host.Build();
+await builder.RunAsync();
---
+++ VectorStoreSearchDemo/RagChatService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+
+namespace VectorStoreSearchDemo
+{
+    class RagChatService(
+            Kernel kernel
+        ) : IHostedService {
+        public async Task StartAsync(CancellationToken cancellationToken) {
+
+            while (!cancellationToken.IsCancellationRequested) {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("User：");
+                var question = Console.ReadLine();
+
+                var response = kernel.InvokePromptStreamingAsync(
+                        promptTemplate: """
+                            你只能说“中文”！    
+                            Question: {{question}}
+                        """,
+                        arguments: new KernelArguments() {
+                            {"question", question }
+                        },
+                        templateFormat: "handlebars",
+                        promptTemplateFactory: new HandlebarsPromptTemplateFactory()
+                    );
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\nAssistant > ");
+
+                try {
+                    await foreach (var message in response.ConfigureAwait(false)) {
+                        Console.Write(message);
+                    }
+                    Console.WriteLine();
+                } catch (Exception ex) {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Call to LLM failed with error: {ex}");
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) {
+            return Task.CompletedTask;
+        }
+    }
+}
